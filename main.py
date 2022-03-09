417171b5b89d3cc36e8bcc983d0b0038b41d4288
Isaac Silva <isaacandroid180320@gmail.com>
2022-03-09 11:02:52 -0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyautogui
 import pyperclip
 import time
+import requests
+import sys 
+import os
 
 #Encontrar posição do mouse
 def ver_posicao():
@@ -8,33 +11,50 @@ def ver_posicao():
     mouse = pyautogui.position()
     print(mouse)
 
-# 1 abrir o chrome
-pyautogui.hotkey("win") #hotkey é atalho
-pyperclip.copy("chrome")
-pyautogui.hotkey("ctrl","v")
-time.sleep(2)
-pyautogui.press("enter") #press é para clicar
-time.sleep(8)
+try:
+    db = sys.argv[1]
+    dl = sys.argv[2]
+except Exception:
+    #Caso queira mudar a db e o delimitador
+    db = "db.txt"
+    dl = "|"
 
-# 2 abrir guia anonima
-pyautogui.hotkey("ctrl","shift","n")
-time.sleep(1)
+    f = open(db,'r').readlines()
 
-# 3 pesquisar pagina de login outlook
-pyperclip.copy("https://login.live.com/login.srf")
-pyautogui.hotkey("ctrl","v")
-pyautogui.press("enter")
-time.sleep(3)
+    for i in range(len(f)):
+        email = f[i].split()[0].split(dl)[0]
+        senha = f[i].split()[0].split(dl)[1]
 
-# 4 Fazer Login
-pyperclip.copy("")# e-mail
-pyautogui.hotkey("ctrl","v")
-time.sleep(1)
-pyautogui.click(x=809, y=506)
-time.sleep(2)
-pyperclip.copy("")# senha
-pyautogui.hotkey("ctrl","v")
-time.sleep(1)
-pyautogui.click(x=817, y=539)
-time.sleep(2)
-pyautogui.press("enter")
+        print(email+"|"+senha)
+
+
+        # 1 abrir o chrome
+        pyautogui.hotkey("win") #hotkey é atalho
+        pyperclip.copy("chrome")
+        pyautogui.hotkey("ctrl","v")
+        time.sleep(5)
+        pyautogui.press("enter") #press é para clicar
+        time.sleep(15)
+
+        # 2 abrir guia anonima
+        pyautogui.hotkey("ctrl","shift","n")
+        time.sleep(2)
+
+        # 3 pesquisar pagina de login outlook
+        pyperclip.copy("https://login.live.com/login.srf")
+        pyautogui.hotkey("ctrl","v")
+        pyautogui.press("enter")
+        time.sleep(5)
+
+        # 4 Fazer Login
+        pyperclip.copy(email)# e-mail
+        pyautogui.hotkey("ctrl","v")
+        time.sleep(1)
+        pyautogui.click(x=809, y=506)
+        time.sleep(2)
+        pyperclip.copy(senha)# senha
+        pyautogui.hotkey("ctrl","v")
+        time.sleep(1)
+        pyautogui.click(x=817, y=539)
+        time.sleep(2)
+        pyautogui.press("enter")
